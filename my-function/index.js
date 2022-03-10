a9970bce5fb6e7e86fb0f03e00f36d81e9ef5dd5
Jonathan Tea <z5146699@ad.unsw.edu.au>
2022-03-10 19:28:29 +1100

    p

--- my-function/index.js
+++ my-function/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('HelLO!'),
+        body: JSON.stringify('NEWS'),
     };
     return response;
 };
